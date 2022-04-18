80ff2dc9b6e290df08f22fe7e7dfe9aa4ae604eb
christopher1664 <christopher.sostre1@stu.bmcc.cuny.edu>
2022-04-18 16:59:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
-# make library
+# Make the library
 library = {}
-# name shelf
+#######################################
+# Name shelf
 shelf = input("name this shelf: ")
+# Space for book
+library[shelf] = {}
+book = input("name this book: ")
+# Space for description and author
+library[shelf][book] = []
+d = input("description for book: ")
+a = input("Author for book: ")
+library[shelf][book].append(d)
+library[shelf][book].append(a)
 
-#space for shelf
-library[shelf] = []
-#space for book, description and author
-library[shelf].append([])
-book = input("put a book here: ")
-description = input("put a description: ")
-author = input("put a Author: ")
-library[shelf][0].append(book)
-library[shelf][0].append(description)
-library[shelf][0].append(author)
-#  NOTE: shelf is a string NOT an index
-# a for loop to keep doing this key would be 0, 1, ect.
-library[shelf].append([])
-book = input("put a book here: ")
-description = input("put a description: ")
-author = input("put a Author: ")
-library[shelf][1].append(book)
-library[shelf][1].append(description)
-library[shelf][1].append(author)
+# assume 'a' was your shelf name
+# print(library['a'][book][0])
+# print(library['a'][book][1])
+#######################################
+#^
+#|
+# to make another shelf you need to do this process again
+
 
 #for loop here if you want
 
-print(library)
-# how to grab the description of book 2
-print(library[shelf][1][1])
-# how to grab the author of book 1
-print(library[shelf][0][2])
-print(library[shelf])
 
 # library['shelf'] -> {[book1... dic1...],[],[]....} everything in shelf
 # library['shelf'][0] -> book1... dic1... book1's stuff
