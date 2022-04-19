2850399fc071e1c148be377279659fa2dc69aad0
christopher1664 <christopher.sostre1@stu.bmcc.cuny.edu>
2022-04-18 20:34:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
-# Make the library
+import csv
+start_time = time.time()
+# Open a file for writing
+with open('csv file name here', newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
 library = {}
-#######################################
-# Name shelf
-shelf = input("name this shelf: ")
-# Space for book
+#make the shelf
+shelf = 'shelf1'
 library[shelf] = {}
-book = input("name this book: ")
-# Space for description and author
-library[shelf][book] = []
-description = input("description for book: ")
-author = input("author for book: ")
-library[shelf][book].append(d)
-library[shelf][book].append(a)
+for n in range(-1, len(data[0])):
+    ########################################
+    # Space for book
+    book = data[n][0]
+    # Space for description and author
+    library[shelf][book] = []
+    description = data[n][1]
+    author = data[n][2]
+    library[shelf][book].append(description)
+    library[shelf][book].append(author)
+    #######################################
+    #^
+    #|
+    # to make another shelf you need to do this process again
 
-# assume 'a' was your shelf name
-# print(library['a'][book][0])
-# print(library['a'][book][1])
 #######################################
-#^
-#|
-# to make another shelf you need to do this process again
-
-#for loop here if you want
+# user input for book
+box = []
+x = 1
+print('choose a book')
+for key in library[shelf]:
+    print(f"{x}. {key}")
+    x = x + 1
+    box.append(key)
+x = x - 1
+i = int(input())
+while i > x or i < 1:
+    print(" not a choice")
+    i = int(input())      
+i = i-1
+print(f"You chose, {box[i]}")
+# description
+print(library[shelf][box[i]][0])
+# author
+print(f"by {library[shelf][box[i]][1]}")
+#######################################
+#! Find a way to pop a book from shelf
 
 # library['shelf'] -> {[book1... dic1...],[],[]....} everything in shelf
 # library['shelf'][0] -> book1... dic1... book1's stuff
