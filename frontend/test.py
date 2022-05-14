cf8c6cd854d070ad529a29ae1509422c972726ff
Alan Yu <alanyu108@gmail.com>
2022-05-14 15:13:25 -0400

    added a display for books and buttons where users can select the books, still need to send book info to other widgets

--- frontend/test.py
+++ frontend/test.py
@@ -1,3 +1,4 @@
+from email.charset import QP
 import sys
 
 
@@ -9,13 +10,10 @@ from PyQt5.QtWidgets import QGridLayout
 from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QGroupBox
+from PyQt5.QtWidgets import QGroupBox, QRadioButton, QButtonGroup
 from PyQt5 import QtCore
 from PyQt5 import QtGui
-
-
-
-
+from PyQt5 import QtWidgets
 
 # Create a subclass of QMainWindow to setup the calculator's GUI
 class LibUi(QMainWindow):
@@ -32,34 +30,63 @@ class LibUi(QMainWindow):
         self._centralWidget = QWidget(self)
         self.setCentralWidget(self._centralWidget)
         self._centralWidget.setLayout(self.generalLayout)
-        self._createGrid()
+        self.BookWidget = QtWidgets.QScrollArea(self)
+        self.BookWidget.setGeometry(QtCore.QRect(10, 20, 600,400))
+        
+        #adds all the books retrieved from the back end to book widget
+        self.data = self.getLibraryInfo()
+        self.ButttonGroup  = QButtonGroup(self);
+
+        self.ButttonGroup.buttonClicked[int].connect(self.show_content)
+
+        self.BookGridLayout = self._createBookGrid()
+        self.BookWidget.setLayout(self.BookGridLayout)
+        
         
         
        
 
     def getLibraryInfo(self):
-        data = {'shelf1': {'Curious George':['monkey', 'Christopher'], 
-                          'Henry and Mudge': ['About a boy and his dog', 'Bob'], 
-                          'Diary of a wimpy kid': ['kid writes diary', 'Daniel'], 
-                          'Bone': ['human bones', 'Michelle']
-                          },'shelf2': {'Curious George':['monkey', 'Christopher'], 
-                          'Henry and Mudge': ['About a boy and his dog', 'Bob'], 
-                          'Diary of a wimpy kid': ['kid writes diary', 'Daniel'], 
-                          'Bone': ['human bones', 'Michelle']
-                          }
+        # data = {'shelf1': {'Curious George':['monkey', 'Christopher'], 
+        #                   'Henry and Mudge': ['About a boy and his dog', 'Bob'], 
+        #                   'Diary of a wimpy kid': ['kid writes diary', 'Daniel'], 
+        #                   'Bone': ['human bones', 'Michelle']
+        #                   }
+        #                   ,'shelf2': {'Curious George':['monkey', 'Christopher'], 
+        #                   'Henry and Mudge': ['About a boy and his dog', 'Bob'], 
+        #                   'Diary of a wimpy kid': ['kid writes diary', 'Daniel'], 
+        #                   'Bone': ['human bones', 'Michelle']
+        #                   }
+        #                 #   ,'shelf3': {'Curious George':['monkey', 'Christopher'], 
+        #                 #   'Henry and Mudge': ['About a boy and his dog', 'Bob'], 
+        #                 #   'Diary of a wimpy kid': ['kid writes diary', 'Daniel'], 
+        #                 #   'Bone': ['human bones', 'Michelle']
+        #                 #   } }
+
+        data = {'Shelf1': {'Catcher in the Rye': ['Fiction', 'J.D. Salinger'],
+            'Don Quixote': ['Fiction', 'Miguel de Cervantes'],
+            'One Hundred Years of Solitude': ['Fiction',
+                                              'Gabriel Garcia Marquez']},
+                'Shelf2': {'Hamlet': ['Fiction', 'William Shakespeare'],
+                            'Moby Dick': ['Fiction', 'Herman Melville'],
+                            'Odyssey': ['Fiction', 'Homer'],
+                            'The Great Gatsby': ['Fiction', 'F.Scott Fitzgerald'],
+                            'War and Peace ': ['Fiction', 'Leo Tolstoy']},
+                'Shelf3': {'The Complete Works of Plato': ['Non Fiction', 'Plato'],
+                            'The Diary of a Young Girl': ['Non fiction', 'Anne Frank'],
+                            'Walden': ['Non fiction', 'Henry David Thoreau']}}
                           
-              }
         return data
 
    
     
-    def _createGrid(self):
-        """Create the buttons."""
+    def _createBookGrid(self):
         self.icons = {}
         GridLayout = QGridLayout()
+        counter = 0
     
-        data = self.getLibraryInfo()
-        for row, books in data.items():
+        # data = self.getLibraryInfo()
+        for row, books in self.data.items():
             row = int(row[-1]) - 1
             for col, info in enumerate(books.items()):
                 bookName = info[0]
@@ -75,25 +102,46 @@ class LibUi(QMainWindow):
             
 
                 box = QGroupBox(bookName)
-                box.setCheckable(True)
-                
-
                 boxLayout = QVBoxLayout()
-            
                 box.setLayout(boxLayout)
+            
+              
+            
 
                 boxLayout.addWidget(image)
+
+####################################################
+                button = QRadioButton()
+                button.setText("")
+                self.ButttonGroup.addButton(button, counter);
+                counter+=1
+                boxLayout.addWidget(button)
+
+
+
+
                 self.icons[bookName] =  box
+
                 
                 
-                # text = QLabel(bookName)
-                # GridLayout.addWidget(text, row, col)
+                
+              
                 
                 GridLayout.addWidget(self.icons[bookName], row, col)
-            
-        self.generalLayout.addLayout(GridLayout, 0, 0)
+        GridLayout.setGeometry(QtCore.QRect(530, 20, 271, 171))
+        
+
+        return GridLayout
 
         
+    def show_content(self, id):
+        counter = 0
+        for _, books in self.data.items():
+            for _, info in enumerate(books.items()):
+                if counter == id:
+                    print(info)
+                counter+=1
+
         
 
 
